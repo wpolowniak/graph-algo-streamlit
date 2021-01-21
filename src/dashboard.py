b896d8851df33080aa687a92d17475ad278034c4
wpolowniak <39831423+wpolowniak@users.noreply.github.com>
2021-01-21 10:51:36 -0500

    Added ability to upload your own file, or use the default data included in the new ./data/default_data.xlsx file

--- src/dashboard.py
+++ src/dashboard.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import pandas as pd
-import altair as alt
 import base64
 from io import BytesIO
 from analysis import *
 from visualizations import *
+from pathlib import Path
 
 def to_excel(df):
     output = BytesIO()
@@ -29,62 +29,68 @@ st.write("""* **Perfect Subset** - when *all* of a panel's genes are contained i
 * **Overlap** - when a panel and its perfect subset both fall into the same panel size category (small, medium, large).""")
 
 # Instantiate the sidebar
-uploaded_file = st.sidebar.file_uploader('Choose an xlsx file', type='xlsx')
+uploaded_file = st.sidebar.file_uploader('Choose an xlsx file, otherwise using example data.', type='xlsx')
 # sliders
 small_slider = st.sidebar.slider('Select Max Number of Genes for Small Category', value=20)
 medium_slider = st.sidebar.slider('Select Max Number of Genes for Medium Category', value=125, min_value=100, max_value=400)
 # Radio button to select volume
 vol_radio = st.sidebar.radio('Select which Payor volume to analyze:', ['US Institutional Clients', 'US Third Party', 'Both'])
 
+# can uplodad your own data to analyze, if not use default data
 if uploaded_file:
     df = pd.read_excel(uploaded_file)
-    # map in a column with sizes of panels based on slider selections
-    df_with_sizes = df.copy()
-    df_with_sizes['Panel_Size'] = df_with_sizes['num_genes_whitepaper'].apply(sizes, args=(small_slider, medium_slider))
+else:
+    loc = Path.cwd() / 'data' / 'default_data.xlsx'
+    df = pd.read_excel(loc)
 
-    graph_obj = create_graph(df)
-    # total output file
-    output_df = run_analysis(graph_obj, df, vol_radio, small_slider, medium_slider)
-    # overlap only file
-    overlap_df = output_df.loc[output_df['overlap'] == 'yes']
-    # ---------------------------------------------------------------------
-    # Display outputs
 
-    # Display Graphs
-    st.header(f"Volume Relationships of {len(output_df.loc[output_df['overlap'] == 'yes'])} Panels with Perfect Subsets and Overlap")
-    st.write(perfect_subset_overlap_summary(overlap_df))
-    st.write(parent_child_overlap_scatter(overlap_df))
+# map in a column with sizes of panels based on slider selections
+df_with_sizes = df.copy()
+df_with_sizes['Panel_Size'] = df_with_sizes['num_genes_whitepaper'].apply(sizes, args=(small_slider, medium_slider))
 
-    # Visualize total panels by size
-    st.header('Number of Panels in Each Bucket')
-    st.write(total_panels_by_size_bar(df, small_slider, medium_slider))
+graph_obj = create_graph(df)
+# total output file
+output_df = run_analysis(graph_obj, df, vol_radio, small_slider, medium_slider)
+# overlap only file
+overlap_df = output_df.loc[output_df['overlap'] == 'yes']
+# ---------------------------------------------------------------------
+# Display outputs
 
-    # Horizontal Summary Chart
-    st.header('Summary Stats')
-    st.write(perfect_subset_summary_stats(output_df))
+# Display Graphs
+st.header(f"Volume Relationships of {len(output_df.loc[output_df['overlap'] == 'yes'])} Panels with Perfect Subsets and Overlap")
+st.write(perfect_subset_overlap_summary(overlap_df))
+st.write(parent_child_overlap_scatter(overlap_df))
 
-    # Vertical Overlap Bar Chart
-    st.header('Count of Overlap by Panel Size')
-    st.write(count_of_overlap_by_panel_size(overlap_df))
-    
-    # Display Output files - Full output
-    st.header('Outputs')
-    st.write('Full Output of Perfect Subsets')
-    output_df
-    # To download an xlsx file (also need above two functions)
-    if 'output_df' in locals():  
-        st.markdown(get_table_download_link(output_df), unsafe_allow_html=True)
+# Visualize total panels by size
+st.header('Number of Panels in Each Bucket')
+st.write(total_panels_by_size_bar(df, small_slider, medium_slider))
 
-    # Overlap file only
-    st.write('Overlap Only')
-    overlap_df
-    # To download an xlsx file (also need above two functions)
-    if 'overlap_df' in locals():  
-        st.markdown(get_table_download_link(overlap_df), unsafe_allow_html=True)
+# Horizontal Summary Chart
+st.header('Summary Stats')
+st.write(perfect_subset_summary_stats(output_df))
 
-    # Original input file, with sizes mapped in
-    st.write('Original file, with sizes mapped in')
-    df_with_sizes
-    # To download an xlsx file (also need above two functions)
-    if 'df_with_sizes' in locals():  
-        st.markdown(get_table_download_link(df_with_sizes), unsafe_allow_html=True)
+# Vertical Overlap Bar Chart
+st.header('Count of Overlap by Panel Size')
+st.write(count_of_overlap_by_panel_size(overlap_df))
+
+# Display Output files - Full output
+st.header('Outputs')
+st.write('Full Output of Perfect Subsets')
+output_df
+# To download an xlsx file (also need above two functions)
+if 'output_df' in locals():  
+    st.markdown(get_table_download_link(output_df), unsafe_allow_html=True)
+
+# Overlap file only
+st.write('Overlap Only')
+overlap_df
+# To download an xlsx file (also need above two functions)
+if 'overlap_df' in locals():  
+    st.markdown(get_table_download_link(overlap_df), unsafe_allow_html=True)
+
+# Original input file, with sizes mapped in
+st.write('Original file, with sizes mapped in')
+df_with_sizes
+# To download an xlsx file (also need above two functions)
+if 'df_with_sizes' in locals():  
+    st.markdown(get_table_download_link(df_with_sizes), unsafe_allow_html=True)
