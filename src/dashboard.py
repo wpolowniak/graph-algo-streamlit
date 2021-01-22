c9b42c4e46bef093b442f48f2d05869462e3ec75
wpolowniak <39831423+wpolowniak@users.noreply.github.com>
2021-01-22 14:10:45 -0500

    removed engine from pd.ExcelWriter

--- src/dashboard.py
+++ src/dashboard.py
@@ -8,7 +8,7 @@ from pathlib import Path
 
 def to_excel(df: pd.DataFrame):
     output = BytesIO()
-    writer = pd.ExcelWriter(output, engine='openpyxl')
+    writer = pd.ExcelWriter(output)
     df.to_excel(writer, sheet_name='Sheet1', index=False)
     writer.save()
     processed_data = output.getvalue()
